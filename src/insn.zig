d54376d4c86274ab599dc89e3f86c8021b063fd1
SoraTenshi <dream@neoncity.dev>
2024-07-07 18:06:34 +0200

    fix: added sentinels

--- src/insn.zig
+++ src/insn.zig
@@ -1,11 +1,13 @@
 const arch = @import("arch/arch.zig");
 
+const zeroes = @import("std").mem.zeroes;
+
 pub const Detail = extern struct {
     regs_read: [20]u16,
     regs_read_count: u8,
     regs_write: [20]u16,
     regs_write_count: u8,
-    groups: [8]u8,
+    groups: [8:0]u8 = zeroes([8:0]u8),
     groups_count: u8,
     writeback: bool,
     arch: arch.Arch,
@@ -15,8 +17,8 @@ pub const Insn = extern struct {
     id: c_uint,
     address: usize,
     size: u16,
-    bytes: [24]u8,
-    mnemonic: [32]u8,
-    op_str: [160]u8,
+    bytes: [24:0]u8 = zeroes([24:0]u8),
+    mnemonic: [32:0]u8 = zeroes([32:0]u8),
+    op_str: [160:0]u8 = zeroes([160:0]u8),
     detail: ?*Detail,
 };
